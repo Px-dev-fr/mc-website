53fbb6862f9fe8641c01640d662ae6ae388b09fd
Bertrand Petit <petit@prestaflex.ch>
2022-06-09 16:57:05 +0200

    ajout read more block

--- wp/wp-content/themes/multicredit/template-credit-full.php
+++ wp/wp-content/themes/multicredit/template-credit-full.php
@@ -1,14 +1,14 @@
-
 <?php
+
 /**
-* Template Name: Credit Page
-*/
+ * Template Name: Credit Page
+ */
 ?>
 
 <?php get_header(); ?>
 
 <?php // Add Calculator component
- echo get_template_part('template-parts/credit-calculator-component-min');
+echo get_template_part('template-parts/credit-calculator-component-min');
 ?>
 
 
@@ -16,29 +16,33 @@
 	<div class="row">
 		<div class="col-lg-12">
 			<?php
-			if ( have_posts() ) {
-				while ( have_posts() ) {
+			if (have_posts()) {
+				while (have_posts()) {
 					the_post();
 
 					// Displays the page's content
 					the_content();
-
 				} // end while
 			} // end if
 			?>
 
-			<?php // Add Calculator link block
-			 echo get_template_part('template-parts/credit-calculator-link-block');
+			<?php // Add Read More block
+			echo get_template_part('template-parts/read-more-block');
 			?>
 
 
-      <?php // Add Blog block
-        echo get_template_part('template-parts/credit-blog-block');
-      ?>
+			<?php // Add Calculator link block
+			echo get_template_part('template-parts/credit-calculator-link-block');
+			?>
+
+
+			<?php // Add Blog block
+			echo get_template_part('template-parts/credit-blog-block');
+			?>
 
 
 			<?php // Add Contact block
-				echo get_template_part('template-parts/credit-contact-block');
+			echo get_template_part('template-parts/credit-contact-block');
 			?>
 
 		</div>
